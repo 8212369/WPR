c27014ec8ba936a15f6ad4a44c0508bf5ae1ecc3
Hector Cachoulet <hector@cachoulet.com>
2022-12-06 23:25:47 +0100

    Add stub for EmailComposeTask (add support for Orbital)

---
+++ Core/Microsoft.Phone/Tasks/EmailComposeTask.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Microsoft.Phone.Tasks
+{
+    public class EmailComposeTask
+    {
+        public EmailComposeTask()
+        {
+
+        }
+        public string Bcc { get; set; }
+
+        public string Body { get; set; }
+        public string Cc { get; set; }
+
+
+        public Nullable<int> CodePage { get; set; }
+        public string Subject { get; set; }
+        public string To { get; set; }
+        public void Show()
+        {
+        }
+    }
+}
